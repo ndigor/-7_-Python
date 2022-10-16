e6adfac7337ca9abf5d7ef49eecfe7d6640292c8
ndigor <108065561+ndigor@users.noreply.github.com>
2022-10-16 17:36:03 +0300

    Описана базовая структура телеграмм-бота

---
+++ src/startup.py
@@ -0,0 +1,15 @@
+import configurations.configure as cfg
+
+
+def startup():
+    """
+    Configure the application.
+    """
+    cfg.add_env()
+    cfg.add_logging()
+    cfg.add_telegram_bot()
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Priva
